d77d6f7a9173c0f28e5384002fca5a057602caac
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-04-29 17:50:25 +0530

    client and backend in the same port

--- app.js
+++ app.js
@@ -45,10 +45,14 @@ app.use(cookieParser());
 
 app.use(cors());
 app.options('*', cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
