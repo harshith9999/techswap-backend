29e73a3080db367bff2c77cba47056fad22ef99f
yaswanthkapollo <yaswanth.k@apollo247.org>
2021-05-20 19:12:01 +0530

    env file included

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Pages.css";
 import axios from "axios";
-const BASE_URL = `http://localhost:8080`
+const BASE_URL = process.env.BASE_URL
 class App extends Component {
   async postData() {
     try {
